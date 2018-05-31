d085b461cfa4a332f8a83f24c545e4f2f5d448ee
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-05-31 15:35:42 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {
+        ...state,
+        friends: [
+          ...state.friends,
+          {
+            name: action.friend.name,
+            hometown: action.friend.hometown,
+            id: action.friend.id
+          }
+        ]
+      };
+    case "REMOVE_FRIEND":
+      let copy = [...state.friends];
+      let newFriends = copy.filter(friend => friend.id !== action.id);
+      return {
+        ...state,
+        friends: newFriends
+      };
+    default:
+      return state;
+  }
 }
